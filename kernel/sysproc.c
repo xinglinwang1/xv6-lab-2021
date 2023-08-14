a3072a289b7c16c25b9972d286b8fa23693d8f0a
wangxinglin <979705352@qq.com>
2023-08-14 12:51:06 +0800

    lab4 commit has wrong

--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -57,7 +57,7 @@ sys_sleep(void)
 {
   int n;
   uint ticks0;
-
+  backtrace();
   if(argint(0, &n) < 0)
     return -1;
   acquire(&tickslock);
@@ -94,4 +94,28 @@ sys_uptime(void)
   xticks = ticks;
   release(&tickslock);
   return xticks;
+}
+
+uint64
+sys_sigreturn(void)
+{
+  struct proc *p = myproc();
+  p->sigreturned = 1;
+  *(p->trapframe) = p->alarmtrapframe;
+  usertrapret();
+  return 0;
+}
+
+uint64
+sys_sigalarm(void)
+{
+  int ticks;
+  uint64 handler;
+  struct proc *p = myproc();
+  if(argint(0, &ticks) < 0 || argaddr(1, &handler) < 0)
+    return -1;
+  p->alarminterval = ticks;
+  p->alarmhandler = (void (*)())handler;
+  p->alarmticks = 0;
+  return 0;
 }
